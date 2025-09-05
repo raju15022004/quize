f5c16f5a03b38605214f9b24b77e730010d8125e
raju15022004 <raju032004@gmail.com>
2025-09-05 12:53:40 +0530

    Quize.jsx

--- src/Components/Quize/Quize.jsx
+++ src/Components/Quize/Quize.jsx
@@ -1,13 +1,13 @@
 import React, { useState, useRef } from 'react';
-
 import './Quize.css'
 import {data} from '../../assets/data';
 
 function Quize() {
-  let [index,setIndex]=useState(1);
+  let [index,setIndex]=useState(0);
   let [question,setQuestion]=useState(data[index]);
   let [lock,setLock]=useState(false);
   let [score,setScore]=useState(0);
+  let [result,setResult]=useState(false);
 
   let Option1=useRef(null);
   let Option2=useRef(null);
@@ -21,6 +21,7 @@ function Quize() {
      if(question.ans===ans){
       e.target.classList.add("correct");
       setLock(true);
+      setScore(prev=>prev+1);
     }
     else{
       e.target.classList.add("wrong");
@@ -32,21 +33,49 @@ function Quize() {
 
   }
   const next=()=>{
-
+    if(lock===true){
+      if(index===data.length-1){
+        setResult(true);
+        return 0;
+      }
+      setIndex(++index);
+      setQuestion(data[index]);
+      setLock(false);
+      option_array.map((Option)=>{
+        Option.current.classList.remove("wrong");
+        Option.current.classList.remove("correct");
+        return null;
+      })
+    }
   }
+
+  const reset=()=>{
+    setIndex(0);
+    setQuestion(data[0]);
+    setScore(0);
+    setLock(false);
+    setResult(false);
+  }
+
   return (
     <div className='container'>
       <h1>Quize App</h1>
       <hr/>
-      <h2>{index+1}.{question.question}</h2>
+      {result?<></>:<><h2>{index+1}.{question.question}</h2>
       <ul>
         <li ref={Option1} onClick={(e)=>{checkAns(e,1)}}>{question.option1}</li>
         <li ref={Option2} onClick={(e)=>{checkAns(e,2)}}>{question.option2}</li>
         <li ref={Option3} onClick={(e)=>{checkAns(e,3)}}>{question.option3}</li>
         <li ref={Option4} onClick={(e)=>{checkAns(e,4)}}>{question.option4}</li>
       </ul>
-      <button>Next</button>
-      <div className='index'>1 of 5 questions</div>
+      <button onClick={next}>Next</button>
+      <div className='index'>{index+1} of {data.length} questions</div>
+      </>}
+      {result?<>
+      <h2>You Scored {score} out of {data.length}</h2>
+      <button onClick={reset}>Reset</button>
+      </>:<></>}
+
     </div>
   )
 }
